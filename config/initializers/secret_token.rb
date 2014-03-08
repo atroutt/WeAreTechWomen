9fc959c2cc254cccccb06313db112efbd653c06c
atroutt <audrey.troutt@gmail.com>
2014-03-08 12:48:19 -0500

    Update secret token again.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-config.secret_token = SETTINGS[:secret_token] || '18e705437828204437e3b364ce33abc4c74f5dd5a624d7977121ce0986f5be2089fed7175bcc950f29fc5e4ea56c71da2da3efe946ae2014ad743eb5d13bebf4'
-config.secret_key_base = ENV['SECRET_KEY_BASE'] ||'18e705437828204437e3b364ce33abc4c74f5dd5a624d7977121ce0986f5be2089fed7175bcc950f29fc5e4ea56c71da2da3efe946ae2014ad743eb5d13bebf4'
+Citizenry::Application.config.secret_token = SETTINGS[:secret_token] || '18e705437828204437e3b364ce33abc4c74f5dd5a624d7977121ce0986f5be2089fed7175bcc950f29fc5e4ea56c71da2da3efe946ae2014ad743eb5d13bebf4'
+Citizenry::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] ||'18e705437828204437e3b364ce33abc4c74f5dd5a624d7977121ce0986f5be2089fed7175bcc950f29fc5e4ea56c71da2da3efe946ae2014ad743eb5d13bebf4'
