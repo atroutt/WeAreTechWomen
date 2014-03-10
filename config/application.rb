3241a489c1c7b565a835d3ac83b0514d751e39ab
Cassandra King <cking@Administrators-MacBook-Pro-7.local>
2014-03-10 17:56:24 -0400

    add haml markup homepage, fix #front_nav centering issue, create directories for asset pipeline under assets/images

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module Citizenry
     
     # fix for 3.x rails apps on heroku https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
     config.assets.initialize_on_precompile = false
-
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
